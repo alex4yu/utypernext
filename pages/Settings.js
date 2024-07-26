df03d03dec74c567f1a948b0d75eb4d3faeebdfa
Alex Yu <alexyu2023@gmail.com>
2024-07-26 16:17:49 -0700

    changed settings formatting, added togglable buttons (dont do anything except change on off currently)

--- pages/Settings.js
+++ pages/Settings.js
@@ -9,31 +9,69 @@ export default function Settings(){
     const [noError, setNoError] = useState('OFF');
     const [noBackspace, setNoBackspace] = useState('OFF');
 
+    const toggleSetting = (setting) =>{
+        if(setting === "liveWPM"){
+            setLiveWPM(liveWPM === 'ON' ? 'OFF': 'ON')
+        }
+        else if(setting === "liveAccuracy"){
+            setLiveAccuracy(liveAccuracy === 'ON' ? 'OFF': 'ON')
+        }
+        else if(setting === "trueTyping"){
+            setTrueTyping(trueTyping === 'ON' ? 'OFF': 'ON')
+        }
+        else if(setting === "noError"){
+            setNoError(noError === 'ON' ? 'OFF': 'ON')
+        }
+        else if(setting === "noBackspace"){
+            setNoBackspace(noBackspace === 'ON' ? 'OFF': 'ON')
+        }
+    }
     return(
         <div>
-            <div className = {styles.settingSection} >Display</div>
-                <div className = {styles.settingContainer}>
-                    <div className = {styles.specificSetting}>Live WPM</div>
-                    <div className = {styles.settingButton}></div>
-                </div>
-                <div id = "livePercentDiv" className = {styles.settingContainer}>
-                    <div className = {styles.specificSetting} id = "livePercent">Live Accuracy</div>
-                    <div className = {styles.settingButton}></div>
-                </div>
-                <div id = "trueTypingDiv" className = {styles.settingContainer}>
-                    <div className = {styles.specificSetting} id = "trueTyping">Show True Typing</div>
-                    <div className = {styles.settingButton}></div>
-                </div>
-            <div className = {styles.settingSection} id = "testDiv">Test Settings</div>
-                <div id = "noErrorDiv" className = {styles.settingContainer}>
-                    <div className = {styles.specificSetting} id = "noError">No Errors</div>
-                    <div className = {styles.settingButton}></div>
-                </div>
-                <div id = "noBackspaceDiv" className = {styles.settingContainer}>
-                    <div className = {styles.specificSetting} id = "noBackspace">No Backspace</div>
-                    <div className = {styles.settingButton}></div>
+            <div className = {styles.settingSection}>Display</div>
+            <div className = {styles.sectionContainer}>
+                <div className = {styles.labelContainer}>
+                    <div className = {styles.label}>
+                        <div className = {styles.labelName}>Live WPM</div>
+                        <div className = {styles.labelDescription}>Displays cumulative words per minute during the test </div>
+                    </div>
+                    <div className = {styles.label}>
+                        <div className = {styles.labelName}>Live Accuracy</div>
+                        <div className = {styles.labelDescription}>Displays overall typing accuracy during the test </div>
+                    </div>
+                    <div className = {styles.label}>
+                        <div className = {styles.labelName}>True Typing</div>
+                        <div className = {styles.labelDescription}>Displays actual typed characters rather than prompt during the test </div>
+                    </div>
                     
                 </div>
+                <div className = {styles.buttonsContainer}>
+                    <div className = {styles.settingButton} onClick={()=>toggleSetting('liveWPM')}>{liveWPM}</div>
+                    <div className = {styles.settingButton} onClick={()=>toggleSetting('liveAccuracy')}>{liveAccuracy}</div>
+                    <div className = {styles.settingButton} onClick={()=>toggleSetting('trueTyping')}>{trueTyping}</div>
+                </div>
+            </div>
+
+            <div className = {styles.settingSection}>Typing Settings</div>
+            <div className = {styles.sectionContainer}>
+                <div className = {styles.labelContainer}>
+                    <div className = {styles.label}>
+                        <div className = {styles.labelName}>No Errors</div>
+                        <div className = {styles.labelDescription}>Test ends on first mistake</div>
+                    </div>
+                    <div className = {styles.label}>
+                        <div className = {styles.labelName}>No Backspace</div>
+                        <div className = {styles.labelDescription}>Unable to use backspace in tests</div>
+                    </div>
+                </div>
+                <div className = {styles.buttonsContainer}>
+                    <div className = {styles.settingButton} onClick={()=>toggleSetting('noError')}>{noError}</div>
+                    <div className = {styles.settingButton} onClick={()=>toggleSetting('noBackspace')}>{noBackspace}</div>
+                </div>
+            </div>
+                
+
+           
         </div>
         
         
